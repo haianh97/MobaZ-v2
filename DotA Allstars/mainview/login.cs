6bf8bfe93256d8bf15adc86b314103eba777c81e
Son Hoang <haitac865@gmail.com>
2020-03-29 12:49:35 +0700

    cm

--- DotA Allstars/mainview/login.cs
+++ DotA Allstars/mainview/login.cs
@@ -1,21 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net.Http;
-using System.Diagnostics;
 using DotA_Allstars.mainview;
 using System.Threading;
 using System.Xml;
 using System.IO;
 using System.Text.RegularExpressions;
 using AutoUpdaterDotNET;
-using System.ServiceProcess;
 
 namespace DotA_Allstars
 {
